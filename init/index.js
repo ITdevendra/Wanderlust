07ce3a0790ba5ebdacb5a0a9f938f30e9046375e
Devendra Choudhary <150212134+ITdevendra@users.noreply.github.com>
2026-05-15 11:40:39 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,43 @@
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+// const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+
+// main()
+//     .then(()=>{
+//         console.log("connected to DB");
+//         initDB();
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     });
+
+// async function main(){
+//     await mongoose.connect(MONGO_URL);
+// }
+
+// const initDB = async () =>{
+//     await Listing.deleteMany({});
+//     await Listing.insertMany(initData.data);
+//     console.log("data was initialized");
+// }
+
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+
+async function initDB() {
+  await mongoose.connect(MONGO_URL);
+
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj)=>({...obj, owner:"69fdde050e41be685d539f26"}));
+  await Listing.insertMany(initData.data); // 🌱 insert clean data
+
+  console.log("✅ DB reset done");
+  mongoose.connection.close();
+}
+
+initDB();
---
+++ routes/listing.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+
+const Listing = require("../models/listing.js");
+const {isLoggedIn , isOwner, validateListing} = require("../middleware.js");
+
+const listingController = require("../controllers/listings.js");
+
+const multer = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage});
+
+//index, create route
+router
+  .route("/")
+  .get( wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,  
+    upload.single('listing[image]'),
+    validateListing,
+    wrapAsync(listingController.createListing)
+);
+
+
+//New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm );
+
+//search route
+router.get("/search", wrapAsync(async(req, res) =>{
+    let query = req.query.q;
+
+    const allListings = await Listing.find({
+         $or: [
+            { title: { $regex: query, $options: "i" } },
+            { location: { $regex: query, $options: "i" } },
+            { country: { $regex: query, $options: "i" } }
+        ]
+    });
+    res.render("listings/index", {allListings});
+}));
+
+//show update delete route
+router.route("/:id")
+.get( wrapAsync(listingController.showListing)
+)
+.put(
+    isLoggedIn,
+    isOwner,
+    upload.single('listing[image]'),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+)
+.delete(
+    isLoggedIn, isOwner,
+    wrapAsync(listingController.destroyListing)
+);
+
+//Edit Route
+router.get("/:id/edit", 
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.renderEditForm)
+);
+
+module.exports = router;
